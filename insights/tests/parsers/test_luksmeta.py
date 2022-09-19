5ae6c64d075a145d3b5994e59280c97fd3e672dc
daniel.zatovic <daniel.zatovic@gmail.com>
2022-09-19 16:03:12 +0200

    Extend test coverage
    Signed-off-by: daniel.zatovic <daniel.zatovic@gmail.com>

--- insights/tests/parsers/test_luksmeta.py
+++ insights/tests/parsers/test_luksmeta.py
@@ -1,5 +1,7 @@
 import doctest
+import pytest
 
+from insights import SkipComponent
 from insights.parsers import luksmeta
 from insights.tests import context_wrap
 
@@ -14,9 +16,22 @@ LUKSMETA_OUTPUT = """0   active empty
 7   active cb6e8904-81ff-40da-a84a-07ab9ab5715e
 """  # noqa
 
+LUKSMETA_NOT_FOUND = "bash: luksmeta: command not found..."
+LUKSMETA_NOT_INITIALIZED = "Device is not initialized (./luks1)"
+LUKSMETA_BAD_DEVICE = "./luks2 (LUKS2) is not a LUKSv1 device"
+
 
 def test_luksmeta():
     luksmeta_parsed = luksmeta.LuksMeta(context_wrap(LUKSMETA_OUTPUT))
+
+    with pytest.raises(SkipComponent):
+        luksmeta.LuksMeta(context_wrap(LUKSMETA_NOT_FOUND))
+
+    with pytest.raises(SkipComponent):
+        luksmeta.LuksMeta(context_wrap(LUKSMETA_NOT_INITIALIZED))
+
+    with pytest.raises(SkipComponent):
+        luksmeta.LuksMeta(context_wrap(LUKSMETA_BAD_DEVICE))
 
     assert len(luksmeta_parsed) == 8
 
