921d179ac567a882bd7b6a650b5e54984b94f872
daniel.zatovic <daniel.zatovic@gmail.com>
2022-09-20 10:17:26 +0200

    Fix luksmeta parser documentation
    Signed-off-by: daniel.zatovic <daniel.zatovic@gmail.com>

--- insights/parsers/luksmeta.py
+++ insights/parsers/luksmeta.py
@@ -1,6 +1,6 @@
 """
-luksmeta - command ``luksmeta show -d``
-=======================================
+luksmeta - command ``luksmeta show -d <device_name>``
+=====================================================
 This class provides parsing for the output of luksmeta <device_name>.
 """
 
